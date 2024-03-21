7584fd0f7fc9c1da5641aceb7a5f188d70da5bba
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2024-03-21 08:59:28 -0400

    Shift Right DONE

--- ArraysHomeWork/Main.cpp
+++ ArraysHomeWork/Main.cpp
@@ -1,5 +1,6 @@
 ﻿#include<iostream>
 using namespace std;
+#define tab "/t";
 
 void main()
 {
@@ -52,4 +53,34 @@ void main()
 	}
 	cout << "Максимальное значение: " << max << endl;
 	cout << "Минимальное значение: " << min << endl;
+
+	const int n = 10;
+	int arr[n] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+
+	//Вывод исходного массива на экран
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	//Сдвиг массива:
+	int shift;
+	cout << "Введите количество сдвигов: "; cin >> shift;
+	for (int i = 0; i < shift; i++)
+	{
+		int temp = arr[0];
+		for (int i = 0; i < n; i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+		arr[n - 1] = temp;
+
+		//Вывод сдвинутого массива на экран:
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << tab;
+		}
+		cout << endl;
+	}
 }
--- Shift/Main.cpp
+++ Shift/Main.cpp
@@ -21,12 +21,12 @@ void main()
 	cout << "Введите количество сдвигов: "; cin >> shift;
 	for (int i = 0; i < shift; i++)
 	{
-		int temp = arr[0];
-		for (int i = 0; i < n; i++)
+		int temp = arr[n-1];
+		for (int i = n - 1; i >= 0; i--)
 		{
-			arr[i] = arr[i + 1];
+			arr[i] = arr[i - 1];
 		}
-		arr[n - 1] = temp;
+		arr[0] = temp;
 
 		//Вывод сдвинутого массива на экран:
 		for (int i = 0; i < n; i++)
