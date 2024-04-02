e5cb7e81ac09b8fb0ca19952ea9d6d8504f7e09e
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2024-04-02 03:13:15 -0400

    Random errors prevent DONE

---
+++ Random/Main.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+#define tab "\t"
+
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	int minRand, maxRand;
+	do
+	{
+		cout << "Введите минимальное случайное число: "; cin >> minRand;
+		cout << "Введите максимальное случайное число: "; cin >> maxRand;
+		if (minRand >= maxRand)	cout << "Ошибка. Введите корректные значения: " << endl;
+	} while (minRand >= maxRand);
+
+	//заполнение массива случайными числами:
+	for (int i = 0; i < n; i++)
+	{
+		//arr[i] = rand() % 100;
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+
+	//вывод массива на экран
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	//сортировка массива:
+		
+	for (int i = 0; i < n; i++) //счетчик 'i' выбирает элемент, в который нужно поместить мин значение
+	{
+		for (int j = i + 1; j < n; j++) //счетчик 'j' перебирает оставшиеся элементы
+		{ 
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}			
+		
+	}
+
+	
+	//вывод отсортированного массива на экран
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
---
+++ Unique/Main.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	const int n = 10;
+	int arr[n];
+
+	bool sameValue;
+	for (int i = 0; i < n; )
+	{
+		sameValue = false;
+		int randomValue = rand() % 10 + 20;
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[j] == randomValue) //проверяет есть ли в массиве значение, которое было сгенирировано
+			{
+				sameValue = true;
+				break; //прекращает поиск, так как одинаковое значение было найдено
+			}		
+		}
+		if (sameValue != true) //добавляет значение только при условии, что оно в предыдущих элементах не встречается
+		{
+			arr[i] = randomValue;
+			i++; //сдвиг по элементам происходит только при добавлении нового значения
+		}
+	}
+
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+}
